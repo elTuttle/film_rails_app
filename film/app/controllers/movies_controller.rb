b92500095c27b1863e62a414881a882846461af7
John Tuttle <thejwt@gmail.com>
2017-12-05 13:36:26 +0000

    working on movies controller

--- film/app/controllers/movies_controller.rb
+++ film/app/controllers/movies_controller.rb
@@ -1,2 +1,26 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :description, :actor_ids => [], actor_attributes: [:name,:age,:bio])
+  end
+
 end
--- film/app/models/movie.rb
+++ film/app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
   has_many :actor_movies
   has_many :actors, through: :actor_movies
+
+  accepts_nested_attributes_for :actors, reject_if: proc { |attributes| attributes['name'].blank? }
+
 end
