d837438fbab065f58aac4148bfb5dcd379644f0b
John Tuttle <thejwt@gmail.com>
2017-12-05 16:54:39 +0000

    working on actors controller

--- film/app/controllers/actors_controller.rb
+++ film/app/controllers/actors_controller.rb
@@ -1,2 +1,50 @@
 class ActorsController < ApplicationController
+
+  def index
+    @actors = Actor.all
+  end
+
+  def show
+
+  end
+
+  def new
+    if params[:movie_id]
+      @movie = Movie.find(params[:movie_id])
+      @actor = @movie.actors.build
+      @location = movie_actors_path(@movie,@actor)
+    else
+      @actor = Actor.new
+      @location = actor_path(@actor)
+    end
+  end
+
+  def create
+    if @actor = Actor.create(actor_params)
+      #binding.pry
+      if params[:movie_id]
+        @movie = Movie.find(params[:movie_id])
+        @movie.actors << @actor
+        if @movie.save
+          redirect_to movie_path(params[:movie_id])
+        else
+          flash[:alert] = "Could not save actor"
+          redirect_to new_actor_path
+        end
+      else
+        redirect_to actor_path(@actor)
+      end
+    else
+      flash[:alert] = "Could not save actor"
+      redirect_to new_actor_path
+    end
+  end
+
+
+  private
+
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :age)
+  end
+
 end
